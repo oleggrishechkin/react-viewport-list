c1c0c2ece9f34ac2dcf7b3af8f85e035bd2f6dd8
Oleg Grishechkin <oleggrishechkin@gmail.com>
2022-08-21 22:48:51 +0300

    Fix sroll compensation and cleanup code

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,14 @@ import {
 
 const MIN_INDEX = 0;
 
+const IS_TOUCH = (() => {
+    try {
+        return 'ontouchstart' in window || navigator.maxTouchPoints;
+    } catch (error) {
+        return false;
+    }
+})();
+
 const IS_OVERFLOW_ANCHOR_SUPPORTED = (() => {
     try {
         return window.CSS.supports('overflow-anchor: auto');
@@ -90,7 +98,7 @@ export interface ViewportListRef {
 export interface ViewportListProps<T> {
     viewportRef?: MutableRefObject<HTMLElement | null> | RefObject<HTMLElement | null>;
     items?: T[];
-    itemMinSize: number;
+    itemMinSize?: number;
     margin?: number;
     overscan?: number;
     axis?: 'y' | 'x';
@@ -105,7 +113,7 @@ const ViewportListInner = <T extends any>(
     {
         viewportRef,
         items = [],
-        itemMinSize,
+        itemMinSize = 1,
         margin = 0,
         overscan = 1,
         axis = 'y',
@@ -119,7 +127,7 @@ const ViewportListInner = <T extends any>(
 ) => {
     const propName = axis === 'y' ? PROP_NAME_FOR_Y_AXIS : PROP_NAME_FOR_X_AXIS;
     const maxIndex = items.length - 1;
-    const normalizedItemMinSize = Math.max(0, itemMinSize);
+    const normalizedItemMinSize = Math.max(1, itemMinSize);
     const itemMinSizeWithMargin = normalizedItemMinSize + margin;
     const overscanSize = overscan * itemMinSizeWithMargin;
     const [[startIndex, endIndex], setIndexes] = useState(() => {
@@ -129,16 +137,16 @@ const ViewportListInner = <T extends any>(
     });
     const normalizedStartIndex = normalizeValue(MIN_INDEX, startIndex, maxIndex);
     const normalizedEndIndex = normalizeValue(normalizedStartIndex, endIndex, maxIndex);
-    const topRef = useRef<HTMLDivElement>(null);
-    const bottomRef = useRef<HTMLDivElement>(null);
+    const topSpacerRef = useRef<HTMLDivElement>(null);
+    const bottomSpacerRef = useRef<HTMLDivElement>(null);
     const cacheRef = useRef<Array<number>>([]);
     const scrollToIndexRef = useRef<{
         index: number;
         alignToTop: boolean | ScrollIntoViewOptions;
         offset: number;
     } | null>(startIndex ? { index: startIndex, alignToTop: initialAlignToTop, offset: initialOffset } : null);
-    const scrollCompensationEndIndexRef = useRef(-1);
-    const topStyle = useMemo(
+    const anchorIndexRef = useRef(-1);
+    const topSpacerStyle = useMemo(
         () =>
             getStyle(
                 propName,
@@ -151,12 +159,12 @@ const ViewportListInner = <T extends any>(
             ),
         [normalizedItemMinSize, itemMinSizeWithMargin, normalizedStartIndex, propName]
     );
-    const bottomStyle = useMemo(
+    const bottomSpacerStyle = useMemo(
         () =>
             getStyle(
                 propName,
                 cacheRef.current
-                    .slice(normalizedEndIndex + 1, maxIndex)
+                    .slice(normalizedEndIndex + 1, maxIndex + 1)
                     .reduce(
                         (sum, next) => sum + (next - normalizedItemMinSize),
                         itemMinSizeWithMargin * (maxIndex - normalizedEndIndex)
@@ -166,64 +174,60 @@ const ViewportListInner = <T extends any>(
     );
 
     useRequestAnimationFrame(() => {
-        if (
-            !topRef.current ||
-            !bottomRef.current ||
-            !topRef.current.nextSibling ||
-            !bottomRef.current.previousSibling
-        ) {
+        const viewport = viewportRef ? viewportRef.current : document.documentElement;
+        const topSpacer = topSpacerRef.current;
+        const bottomSpacer = bottomSpacerRef.current;
+
+        if (!viewport || !topSpacer || !bottomSpacer) {
             return;
         }
 
-        const viewport = (viewportRef && viewportRef.current) || document.documentElement;
+        const topElement = topSpacer.nextSibling as Element | null;
+        const bottomElement = bottomSpacer.previousSibling as Element | null;
+
+        if (!topElement || !bottomElement) {
+            return;
+        }
+
         const viewportRect = viewport.getBoundingClientRect();
+        const topElementRect = topElement.getBoundingClientRect();
+        const bottomElementRect = bottomElement.getBoundingClientRect();
         const topLimit =
             normalizeValue(0, viewportRect[propName.top], document.documentElement[propName.clientHeight]) -
             overscanSize;
         const bottomLimit =
             normalizeValue(0, viewportRect[propName.bottom], document.documentElement[propName.clientHeight]) +
             overscanSize;
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const topElementRect = topRef.current.nextSibling.getBoundingClientRect();
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const bottomElementRect = bottomRef.current.previousSibling.getBoundingClientRect();
-        const maxItemsCountInViewPort = itemMinSizeWithMargin
-            ? Math.ceil((bottomLimit - topLimit) / itemMinSizeWithMargin)
-            : items.length;
+        const maxItemsCountInViewPort = normalizeValue(
+            0,
+            Math.ceil((bottomLimit - topLimit) / itemMinSizeWithMargin),
+            items.length
+        );
         let nextStartIndex = startIndex;
         let nextEndIndex = endIndex;
 
-        // If scroll needed
         if (scrollToIndexRef.current) {
             const targetIndex = normalizeValue(MIN_INDEX, scrollToIndexRef.current.index, maxIndex);
 
-            // If our target scroll index is rendered
             if (targetIndex >= startIndex && targetIndex <= endIndex) {
                 let index = startIndex;
-                let element: ChildNode | null = topRef.current.nextSibling;
+                let element: Element | null = topElement;
 
-                while (element && element !== bottomRef.current) {
+                while (element && element !== bottomSpacer) {
                     if (index === targetIndex) {
-                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                        // @ts-ignore
                         element.scrollIntoView(scrollToIndexRef.current.alignToTop);
 
-                        if (
-                            scrollToIndexRef.current.offset &&
-                            Math.abs(viewport[propName.scrollHeight] - viewport[propName.clientHeight]) < 1
-                        ) {
+                        if (scrollToIndexRef.current.offset) {
                             viewport[propName.scrollTop] += scrollToIndexRef.current.offset;
                         }
 
                         scrollToIndexRef.current = null;
 
-                        break;
+                        return;
                     }
 
-                    element = element.nextSibling;
-                    ++index;
+                    index++;
+                    element = element.nextSibling as Element | null;
                 }
 
                 return;
@@ -231,60 +235,63 @@ const ViewportListInner = <T extends any>(
 
             nextStartIndex = targetIndex - maxItemsCountInViewPort;
             nextEndIndex = targetIndex + maxItemsCountInViewPort;
-            // If each item height is fixed
         } else if (fixed) {
             nextStartIndex = Math.floor(
-                (topLimit - topRef.current.getBoundingClientRect()[propName.top]) / itemMinSizeWithMargin
+                (topLimit - topSpacerRef.current.getBoundingClientRect()[propName.top]) / itemMinSizeWithMargin
             );
             nextEndIndex = nextStartIndex + maxItemsCountInViewPort;
-            // If first rendered item is below bottom
         } else if (topElementRect[propName.top] >= bottomLimit) {
+            // fast scroll up
             let diff = topElementRect[propName.top] - bottomLimit;
 
             nextEndIndex = startIndex;
 
             while (diff >= 0 && nextEndIndex > MIN_INDEX) {
-                diff -= (cacheRef.current[--nextEndIndex] || normalizedItemMinSize) + margin;
+                nextEndIndex--;
+                diff -= (cacheRef.current[nextEndIndex] || normalizedItemMinSize) + margin;
             }
 
-            scrollCompensationEndIndexRef.current = startIndex;
+            anchorIndexRef.current = startIndex;
             nextStartIndex = nextEndIndex - maxItemsCountInViewPort;
-            // If last rendered item is above top
         } else if (bottomElementRect[propName.bottom] + margin <= topLimit) {
+            // fast scroll down
             let diff = topLimit - bottomElementRect[propName.bottom] + margin;
 
             nextStartIndex = endIndex;
 
             while (diff >= 0 && nextStartIndex < maxIndex) {
-                diff -= (cacheRef.current[++nextStartIndex] || normalizedItemMinSize) + margin;
+                nextStartIndex++;
+                diff -= (cacheRef.current[nextStartIndex] || normalizedItemMinSize) + margin;
             }
 
             nextEndIndex = nextStartIndex + maxItemsCountInViewPort;
         } else {
-            // If first rendered item is above top
             if (topElementRect[propName.bottom] + margin < topLimit) {
-                ++nextStartIndex;
-                // If first rendered item is below top
+                // scroll down (correction)
+                nextStartIndex++;
             } else if (topElementRect[propName.top] >= topLimit) {
+                // scroll up
                 let diff = topElementRect[propName.top] - topLimit;
 
                 while (diff >= 0 && nextStartIndex > MIN_INDEX) {
-                    diff -= (cacheRef.current[--nextStartIndex] || normalizedItemMinSize) + margin;
+                    nextStartIndex--;
+                    diff -= (cacheRef.current[nextStartIndex] || normalizedItemMinSize) + margin;
                 }
 
-                scrollCompensationEndIndexRef.current = startIndex;
+                anchorIndexRef.current = startIndex;
             }
 
-            // If last rendered item is above bottom
             if (bottomElementRect[propName.bottom] + margin <= bottomLimit) {
+                // scroll down
                 let diff = bottomLimit - bottomElementRect[propName.bottom] - margin;
 
                 while (diff >= 0 && nextEndIndex < maxIndex) {
-                    diff -= (cacheRef.current[++nextEndIndex] || normalizedItemMinSize) + margin;
+                    nextEndIndex++;
+                    diff -= (cacheRef.current[nextEndIndex] || normalizedItemMinSize) + margin;
                 }
-                // If last rendered item is below bottom
             } else if (bottomElementRect[propName.top] > bottomLimit) {
-                --nextEndIndex;
+                // scroll up (correction)
+                nextEndIndex--;
             }
         }
 
@@ -296,23 +303,21 @@ const ViewportListInner = <T extends any>(
                 cacheRef.current = [];
             } else {
                 let index = startIndex;
-                let element: ChildNode | null = topRef.current.nextSibling;
+                let element: Element | null = topElement;
 
-                while (element && index < nextStartIndex && element !== bottomRef.current) {
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-ignore
+                while (element && index < nextStartIndex && element !== bottomSpacer) {
                     cacheRef.current[index++] = element[propName.clientHeight];
-                    element = element.nextSibling;
+                    index++;
+                    element = element.nextSibling as Element | null;
                 }
 
                 index = endIndex;
-                element = bottomRef.current.previousSibling;
+                element = bottomElement;
 
-                while (element && index > nextEndIndex && element !== topRef.current) {
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-ignore
+                while (element && index > nextEndIndex && element !== topSpacer) {
                     cacheRef.current[index++] = element[propName.clientHeight];
-                    element = element.previousSibling;
+                    index++;
+                    element = element.previousSibling as Element | null;
                 }
             }
 
@@ -321,36 +326,45 @@ const ViewportListInner = <T extends any>(
     });
 
     useIsomorphicLayoutEffect(() => {
-        if (scrollCompensationEndIndexRef.current === -1) {
+        const anchorIndex = anchorIndexRef.current;
+
+        anchorIndexRef.current = -1;
+
+        const viewport = viewportRef ? viewportRef.current : document.documentElement;
+        const topSpacer = topSpacerRef.current;
+        const bottomSpacer = bottomSpacerRef.current;
+
+        if (IS_OVERFLOW_ANCHOR_SUPPORTED || anchorIndex === -1 || !viewport || !topSpacer || !bottomSpacer) {
             return;
         }
 
-        if (!topRef.current || !topRef.current.nextSibling || IS_OVERFLOW_ANCHOR_SUPPORTED) {
-            scrollCompensationEndIndexRef.current = -1;
+        const topElement = topSpacer.nextSibling as Element | null;
 
+        if (!topElement) {
             return;
         }
 
-        const viewport = (viewportRef && viewportRef.current) || document.documentElement;
         let index = startIndex;
-        let element: ChildNode | null = topRef.current.nextSibling;
+        let element: Element | null = topElement;
         let sizeDiff = 0;
 
-        while (element && index < scrollCompensationEndIndexRef.current && element !== bottomRef.current) {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
+        while (element && index < anchorIndex && element !== bottomSpacer) {
             sizeDiff += element[propName.clientHeight] - (cacheRef.current[index] || normalizedItemMinSize);
-            element = element.nextSibling;
-            ++index;
+            index++;
+            element = element.nextSibling as Element | null;
         }
 
-        if (sizeDiff && Math.abs(viewport[propName.scrollHeight] - viewport[propName.clientHeight]) < 1) {
-            viewport.style[propName.overflowY] = 'hidden';
-            viewport[propName.scrollTop] += sizeDiff;
-            viewport.style[propName.overflowY] = '';
+        if (sizeDiff) {
+            if (IS_TOUCH) {
+                viewport.style[propName.overflowY] = 'hidden';
+                viewport[propName.scrollTop] += sizeDiff;
+                viewport.style[propName.overflowY] = '';
+            } else {
+                viewport[propName.scrollTop] += sizeDiff;
+            }
         }
 
-        scrollCompensationEndIndexRef.current = -1;
+        anchorIndexRef.current = -1;
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [startIndex]);
 
@@ -366,9 +380,9 @@ const ViewportListInner = <T extends any>(
 
     return (
         <Fragment>
-            <div ref={topRef} style={topStyle} />
+            <div ref={topSpacerRef} style={topSpacerStyle} />
             {items.slice(startIndex, endIndex + 1).map((item, index) => children(item, startIndex + index, items))}
-            <div ref={bottomRef} style={bottomStyle} />
+            <div ref={bottomSpacerRef} style={bottomSpacerStyle} />
         </Fragment>
     );
 };
